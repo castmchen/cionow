e4cbc5086eab8b5806849e5874998e60397bfddb
castmchen <chenqiang_590@163.com>
2019-05-08 11:53:21 +0800

    commit

--- src/app/components/cio-pie/cio-pie.component.ts
+++ src/app/components/cio-pie/cio-pie.component.ts
@@ -7,22 +7,32 @@ import { EChartOption } from 'echarts';
   styleUrls: ['./cio-pie.component.scss']
 })
 export class CioPieComponent implements OnInit {
-  private options: EChartOption = {
-    xAxis: {
-      type: 'category',
-      data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
-    },
-    yAxis: {
-      type: 'value'
-    },
-    series: [
-      {
-        data: [820, 932, 901, 934, 1290, 1330, 1320],
-        type: 'pie'
-      }
-    ]
-  };
-  constructor() {}
+  private options: EChartOption;
+  //  = {
+  //   xAxis: {
+  //     type: 'category',
+  //     data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+  //   },
+  //   yAxis: {
+  //     type: 'value'
+  //   },
+  //   series: [
+  //     {
+  //       data: [820, 932, 901, 934, 1290, 1330, 1320],
+  //       type: 'pie'
+  //     }
+  //   ]
+  // };
+  constructor() {
+    this.options = {
+      series: [
+        {
+          data: [820, 932, 901, 934, 1290, 1330, 1320],
+          type: 'pie'
+        }
+      ]
+    };
+  }
 
   ngOnInit() {}
 }
